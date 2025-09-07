00363ca3b6792e43beb2484f3a701ba5455f07f3
samarth-sachin <samarthpatil@yahoo.com>
2025-09-07 12:35:17 +0530

    basic

---
+++ src/test/java/com/example/AirlineManagement/AirlineManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AirlineManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
